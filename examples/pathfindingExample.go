af0a208817baead50dace8092ba329a0d9a91d8c
Josh Kirkpatrick <josh_kirkpatrick@hotmail.co.uk>
2024-04-08 15:47:07 +0100

    removed tokens from example

--- examples/pathfindingExample.go
+++ examples/pathfindingExample.go
@@ -26,11 +26,11 @@ func pathfindingExample_Test() {
 	// Create a new client with a token
 	options := client.DefaultClientOptions()
 	options.Logger = logger
-	options.Token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiTVlBR0VOVDQiLCJ2ZXJzaW9uIjoidjIuMi4wIiwicmVzZXRfZGF0ZSI6IjIwMjQtMDMtMjQiLCJpYXQiOjE3MTIxNzQxMjUsInN1YiI6ImFnZW50LXRva2VuIn0.b4ICe3ILHtC8P2g4ycOtTbsXO1x8obBAFX6f7TNtltoJ7cImvUYHgEIC8yc8REbXtiu3JTgcbYAuqgP6qoEOzKJlIn_vHuhOZ3AX6OjOQdi6hnlqYq0kF0Vn36CV15Pp8ulgObKGx9zB1SzedLUV5ud77bGZNUNQv5MW8VKGBpqwN3Kv_Eh9dzdyIuKXvD6hMTgC7FlAbVUJE7itThOAnHvX7BBzJY6aiRGdSCbuh07YDRyQ-_28JB4cFC1byXLJ50ZC-3Oh7zdnQFEYXPX3Akv9ntRSQGugXdCiHmDyNdoB_29I6-fYwF1kGw3tXNUvwf6QnndTme5zTREQ0XYZPg"
+	options.Token = "Space Traders Token"
 
 	metricsReporter := metrics.NewMetricsClient(
 		"http://192.168.1.33:8086",
-		"238nUuJVX9CzDqdsU7wvINk8ByIG-3MykZqwUSTEwBIeLgBKNTbwV8x_lik_4t1oXSTfj8OqRPwzmPS8y3tsdg==",
+		"Influx token",
 		"spacetraders",
 		"spacetraders",
 	)
